c87bd359b23d23fa0de2fc57d75c4f0c3fd11e24
MartinJohaa <martin.johansson17@gmail.com>
2014-11-02 16:31:17 +0100

    Tog bort ; i sista locked = med fulkodning! Tog bort onödiga rader!

--- src/com/ioopm/ReadRoomFile.java
+++ src/com/ioopm/ReadRoomFile.java
@@ -27,23 +27,15 @@ public class ReadRoomFile{
 
             // Testar om nextLine tar hela rumsnamnet fram till ; .
             String roomName = rooms.next();
-            // roomName = roomName.substring(0,roomName.length());
             String northConnection = rooms.next();
-            //northConnection = northConnection.substring(0, northConnection.length());
             String eastConnection = rooms.next();
-            //eastConnection = eastConnection.substring(0, eastConnection.length());
             String southConnection = rooms.next();
-            //southConnection = southConnection.substring(0, southConnection.length());
             String westConnection = rooms.next();
-            //westConnection = westConnection.substring(0, westConnection.length());
             String northLock = rooms.next();
-            //northLock = northLock.substring(0, northLock.length());
             String eastLock = rooms.next();
-            //eastLock = eastLock.substring(0, eastLock.length());
             String southLock = rooms.next();
-            //southLock = southLock.substring(0, southLock.length());
-            String westLock = rooms.nextLine();
-            //westLock = westLock.substring(0, westLock.length());
+            String westLock = rooms.nextLine(); // Fråga ass!!
+            westLock = westLock.substring(2, westLock.length());
 
             world[x] = new Room(roomName, northConnection, eastConnection, southConnection, westConnection
                     , northLock, eastLock, southLock, westLock);
