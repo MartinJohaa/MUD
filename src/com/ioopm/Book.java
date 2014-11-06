3e304fbc832eec04eae88e3b6e2b44d43b120931
Erik Englund <erikw.englund@gmail.com>
2014-11-06 16:18:27 +0100

    Changed orde of arguments in book-constructor

--- src/com/ioopm/Book.java
+++ src/com/ioopm/Book.java
@@ -4,12 +4,11 @@ import java.lang.String;
 public class Book extends Items {
     private String title;
     private String author;
-    private int HP;
-    public void Book (int size, String title, String author, int HP){
-        setSize(size);
+    private int yearOfPublishing;
+    public void Book (String title, String author, int yearOfPublishing, int size){
+        super.setSize(size);
         this.title = title;
         this.author = author;
-        this.HP = HP;
     }
 
 
@@ -19,10 +18,6 @@ public class Book extends Items {
 
     public String getAuthor() {
         return this.author;
-    }
-
-    public int getHP() {
-        return this.HP;
     }
 
     public String toString() {
