e39441f55f253678e5ebd0af0ff435503f53d889
Erik Englund <erikw.englund@gmail.com>
2014-11-28 15:19:40 +0100

    started working on tests for Main

--- src/com/ioopm/MainTest.java
+++ src/com/ioopm/MainTest.java
@@ -1,22 +1,30 @@
 package com.ioopm;
 
-import junit.framework.TestCase;
+import junit.framework.*;
+import org.junit.Test;
 
 public class MainTest extends TestCase {
 
+    @Test
     public void testGetSphinxRoomIndex() throws Exception {
-
+        int indexNum = 5;
+        int[] roomIndexArray = {1, 2, 3, 4, 5, 6};
+        assertEquals(Main.getSphinxRoomIndex(indexNum, roomIndexArray), 6);
     }
 
+    @Test
     public void testFindIndex() throws Exception {
-
+        String stringToBeFoundOne = "testString";
+        String stringToBeFoundTwo = "teststring";
+        String[] stringArray = {"hello", "world", "teststring", "testString"};
+        assertEquals(Main.findIndex(stringToBeFoundOne, stringArray), 3);
+        assertEquals(Main.findIndex(stringToBeFoundTwo, stringArray), 2);
     }
-
-    public void testLinkCourses() throws Exception {
-
-    }
+}
+    /*
 
     public void testPlaceKeys() throws Exception {
+        Room[] testRooms = {new Room()};
 
     }
 
@@ -31,4 +39,4 @@ public class MainTest extends TestCase {
     public void testUnlockDoor() throws Exception {
 
     }
-}
+}*/
